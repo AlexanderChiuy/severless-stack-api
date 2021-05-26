e3b9f886183fb95f1911027d28f20c542f3418dd
Alexander Chiu <achiu2@andrew.cmu.edu>
2021-05-26 09:49:40 -0400

    added get functionality

--- get.js
+++ get.js
@@ -1,35 +1,21 @@
-import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  const data = JSON.parse(event.body);
-
+export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
-    Item: {
-      // The attributes of the item to be created
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
       userId: "123", // The id of the author
-      noteId: uuid.v1(), // A unique uuid
-      content: data.content, // Parsed from request body
-      attachment: data.attachment, // Parsed from request body
-      createdAt: Date.now(), // Current Unix timestamp
+      noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
   }
-}
+
+  // Return the retrieved item
+  return result.Item;
+});
